4300980e1fab0401b14c892e022a16cbed986cfc
“xshsama” <sorrymaker114514@gmail.com>
2025-04-01 14:29:19 +0800

    初始化后端

---
+++ src/main/java/com/xsh/learningtracker/LearningtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.xsh.learningtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xsh/learningtracker/LearningtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsh.learningtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
